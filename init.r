148f6a9b0fe6edbf1678dd00b272ba008cc8479e
David Beauchesne <david.beauchesne@uqar.ca>
2016-10-24 09:28:52 -0400

    first commit

---
+++ init.r
@@ -0,0 +1,24 @@
+# Run init.r before other scripts
+rm(list=ls())
+ # for use in R console.
+ # set own relevant directory if working in R console, otherwise ignore if in terminal
+setwd("/Users/davidbeauchesne/Dropbox/PhD/Misc/Conference_networking")
+# -----------------------------------------------------------------------------
+# PROJECT:
+#    Survey tool that automatically generates results for conference use
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# FUNCTIONS:
+
+# -----------------------------------------------------------------------------
+
+
+
+ShinyApp survey tool
+
+Create shinyApp survey
+    First page:
+
+Each submit button sends a file with the answers to a public dropbox folder
+Changes are dealt with locally on my computer? Or everything could simply be dealt with over the web
