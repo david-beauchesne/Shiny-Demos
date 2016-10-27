4613736ef1090bdc861689be7cbe1c3ffdb24084
David Beauchesne <david.beauchesne@uqar.ca>
2016-10-26 22:55:06 -0400

    minor changes

--- init.r
+++ init.r
@@ -2,7 +2,7 @@
 rm(list=ls())
  # for use in R console.
  # set own relevant directory if working in R console, otherwise ignore if in terminal
-setwd("/Users/davidbeauchesne/Dropbox/PhD/Misc/Conference_networking")
+setwd("/Users/davidbeauchesne/Dropbox/PhD/Misc/SurveyTool")
 # -----------------------------------------------------------------------------
 # PROJECT:
 #    Survey tool that automatically generates results for conference use
@@ -13,12 +13,4 @@ setwd("/Users/davidbeauchesne/Dropbox/PhD/Misc/Conference_networking")
 
 # -----------------------------------------------------------------------------
 
-
-
-ShinyApp survey tool
-
-Create shinyApp survey
-    First page:
-
-Each submit button sends a file with the answers to a public dropbox folder
-Changes are dealt with locally on my computer? Or everything could simply be dealt with over the web
+setwd("./Survey")
