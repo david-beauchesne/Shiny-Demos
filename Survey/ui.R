2d3c71baa0133d3eb6de606091eddde7f7b86b7e
David Beauchesne <david.beauchesne@uqar.ca>
2016-11-07 16:08:10 -0500

    add quizzes

--- Survey/ui.R
+++ Survey/ui.R
@@ -7,35 +7,39 @@ shinyUI(
         # tags$style(type='text/css', '#stressors {color: grey;}'),
 
         navbarPage(
-            title = 'AGA Québec Océan 2016 - Activités sociale',
+            title = 'AGA Québec Océan 2016 - Activités sociales',
 
             tabPanel('Nom d\'équipe',
                 sidebarPanel(width = 3,
-                    h5("Created by:"),
-                    tags$a("Econometrics by Simulation", href="http://www.econometricsbysimulation.com"),
-                    h5("For details on how data is generated:"),
-                    tags$a("Blog Post", href=paste0("http://www.econometricsbysimulation.com/", "2013/19/Shiny-Survey-Tool.html")),
-                    h5("Github Repository:"),
-                    tags$a("Survey-Tool", href=paste0("https://github.com/EconometricsBySimulation/","Shiny-Demos/tree/master/Survey"))
+                    h5("Créé par:"),
+                    h6("Le comité étudiant de Québec Océan"),
+                    h5("Dépôt Github :"),
+                    tags$a("Code source quiz", href=paste0("https://github.com/david-beauchesne/SurveyTool")),
+                    h5("Lien vers l'outil de réseautage :"),
+                    tags$a("AGA 2016 Réseautage", href=paste0("https://bit.ly/AGA-QO2016"))
                 ), #sidebarPanel
 
                 mainPanel(width = 4,
-                    h5('ajouter un titre ici'),
+                    h5('Bienvenue aux activités sociales de l\'AGA 2015 de Québec Océan!'),
+                    h6('Préparées par votre comité étudiant'),
+                    br(),
+                    h5('Commençons par le début, s\'identifier en tant qu\'équipe'),
+                    br(),
                     textInput('participants', 'Veuillez sélectionner un nom pour votre équipe', width = '100%')
                 ) #mainPanel
             ),
 
-            tabPanel('Quiz 1',
+            tabPanel('Membres QO',
                 sidebarPanel(width = 3,
                     # This is intentionally an empty object.
 # !! CHANGE PARAMETER NAME FOR MULTIPLE INSTANCES !!
                     h6(textOutput("save.results_1")),
-                    h5("Created by:"),
-                    tags$a("Econometrics by Simulation", href="http://www.econometricsbysimulation.com"),
-                    h5("For details on how data is generated:"),
-                    tags$a("Blog Post", href=paste0("http://www.econometricsbysimulation.com/", "2013/19/Shiny-Survey-Tool.html")),
-                    h5("Github Repository:"),
-                    tags$a("Survey-Tool", href=paste0("https://github.com/EconometricsBySimulation/","Shiny-Demos/tree/master/Survey")),
+                    h5("Créé par:"),
+                    h6("Le comité étudiant de Québec Océan"),
+                    h5("Dépôt Github :"),
+                    tags$a("Code source quiz", href=paste0("https://github.com/david-beauchesne/SurveyTool")),
+                    h5("Lien vers l'outil de réseautage :"),
+                    tags$a("AGA 2016 Réseautage", href=paste0("https://bit.ly/AGA-QO2016")),
                     # Display the page counter text.
 # !! CHANGE PARAMETER NAME FOR MULTIPLE INSTANCES !!
                     h5(textOutput("counter_1"))
@@ -54,16 +58,97 @@ shinyUI(
             ), #tabPanel
 
 
+            tabPanel('Avantages QO',
+                sidebarPanel(width = 3,
+                    # This is intentionally an empty object.
+# !! CHANGE PARAMETER NAME FOR MULTIPLE INSTANCES !!
+                    h6(textOutput("save.results_2")),
+                    h5("Créé par:"),
+                    h6("Le comité étudiant de Québec Océan"),
+                    h5("Dépôt Github :"),
+                    tags$a("Code source quiz", href=paste0("https://github.com/david-beauchesne/SurveyTool")),
+                    h5("Lien vers l'outil de réseautage :"),
+                    tags$a("AGA 2016 Réseautage", href=paste0("https://bit.ly/QO2015")),
+                    # Display the page counter text.
+# !! CHANGE PARAMETER NAME FOR MULTIPLE INSTANCES !!
+                    h5(textOutput("counter_2"))
+                ), #sidebarPanel
+
+                # Show a table summarizing the values entered
+                mainPanel(width = 7,
+                    # Main Action is where most everything is happenning in the
+                    # object (where the welcome message, survey, and results appear)
+# !! CHANGE PARAMETER NAME FOR MULTIPLE INSTANCES !!
+                    uiOutput("MainAction_2"),
+                    # This displays the action button Next.
+# !! CHANGE PARAMETER NAME FOR MULTIPLE INSTANCES !!
+                    actionButton("Click.Counter_2", "Suivant")
+                ) #mainPanel
+            ), #tabPanel
+
+
+            tabPanel('Océanographie générale',
+                sidebarPanel(width = 3,
+                    # This is intentionally an empty object.
+            # !! CHANGE PARAMETER NAME FOR MULTIPLE INSTANCES !!
+                    h6(textOutput("save.results_3")),
+                    h5("Créé par:"),
+                    h6("Le comité étudiant de Québec Océan"),
+                    h5("Dépôt Github :"),
+                    tags$a("Code source quiz", href=paste0("https://github.com/david-beauchesne/SurveyTool")),
+                    h5("Lien vers l'outil de réseautage :"),
+                    tags$a("AGA 2016 Réseautage", href=paste0("https://bit.ly/QO2015")),
+                    # Display the page counter text.
+            # !! CHANGE PARAMETER NAME FOR MULTIPLE INSTANCES !!
+                    h5(textOutput("counter_3"))
+                ), #sidebarPanel
+
+                # Show a table summarizing the values entered
+                mainPanel(width = 7,
+                    # Main Action is where most everything is happenning in the
+                    # object (where the welcome message, survey, and results appear)
+            # !! CHANGE PARAMETER NAME FOR MULTIPLE INSTANCES !!
+                    uiOutput("MainAction_3"),
+                    # This displays the action button Next.
+            # !! CHANGE PARAMETER NAME FOR MULTIPLE INSTANCES !!
+                    actionButton("Click.Counter_3", "Suivant")
+                ) #mainPanel
+            ), #tabPanel
+
+            tabPanel('Saint-Laurent - Arctique',
+                sidebarPanel(width = 3,
+                    # This is intentionally an empty object.
+            # !! CHANGE PARAMETER NAME FOR MULTIPLE INSTANCES !!
+                    h6(textOutput("save.results_4")),
+                    h5("Créé par:"),
+                    h6("Le comité étudiant de Québec Océan"),
+                    h5("Dépôt Github :"),
+                    tags$a("Code source quiz", href=paste0("https://github.com/david-beauchesne/SurveyTool")),
+                    h5("Lien vers l'outil de réseautage :"),
+                    tags$a("AGA 2016 Réseautage", href=paste0("https://bit.ly/QO2015")),
+                    # Display the page counter text.
+            # !! CHANGE PARAMETER NAME FOR MULTIPLE INSTANCES !!
+                    h5(textOutput("counter_4"))
+                ), #sidebarPanel
+
+                # Show a table summarizing the values entered
+                mainPanel(width = 7,
+                    # Main Action is where most everything is happenning in the
+                    # object (where the welcome message, survey, and results appear)
+            # !! CHANGE PARAMETER NAME FOR MULTIPLE INSTANCES !!
+                    uiOutput("MainAction_4"),
+                    # This displays the action button Next.
+            # !! CHANGE PARAMETER NAME FOR MULTIPLE INSTANCES !!
+                    actionButton("Click.Counter_4", "Suivant")
+                ) #mainPanel
+            ), #tabPanel
+
+
+
             tabPanel('Résultats',
                 column(width = 12,
-                    selectInput(inputId = 'refresh',
-                        label = 'Rafraîchissement automatique',
-                        choices = c('Oui','Non'),
-                        selected = 'Non')
-                ),
-                column(width = 12,
-                    h5('ok ici on va montrer les résultats'),
-                    plotOutput('results', width = "100%", height = "400px")
+                    h5('Résultats par équipe'),
+                    plotOutput('results', width = "100%", height = "600px")
                 )
             )
 
