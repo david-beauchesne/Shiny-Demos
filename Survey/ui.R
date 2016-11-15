06f17ce8258d0461181557840277cf47775a3182
David Beauchesne <david.beauchesne@uqar.ca>
2016-11-14 19:26:51 -0500

    Final version of app. Still has problem with multiple user conflicts when deplooyed on shinyapps.io

--- Survey/ui.R
+++ Survey/ui.R
@@ -87,7 +87,7 @@ shinyUI(
             ), #tabPanel
 
 
-            tabPanel('Océanographie générale',
+            tabPanel('Océanographie générale 1',
                 sidebarPanel(width = 3,
                     # This is intentionally an empty object.
             # !! CHANGE PARAMETER NAME FOR MULTIPLE INSTANCES !!
@@ -115,7 +115,7 @@ shinyUI(
                 ) #mainPanel
             ), #tabPanel
 
-            tabPanel('Saint-Laurent - Arctique',
+            tabPanel('Saint-Laurent - Arctique 1',
                 sidebarPanel(width = 3,
                     # This is intentionally an empty object.
             # !! CHANGE PARAMETER NAME FOR MULTIPLE INSTANCES !!
@@ -140,6 +140,62 @@ shinyUI(
                     # This displays the action button Next.
             # !! CHANGE PARAMETER NAME FOR MULTIPLE INSTANCES !!
                     actionButton("Click.Counter_4", "Suivant")
+                ) #mainPanel
+            ), #tabPanel
+
+            tabPanel('Océanographie générale 2',
+                sidebarPanel(width = 3,
+                    # This is intentionally an empty object.
+            # !! CHANGE PARAMETER NAME FOR MULTIPLE INSTANCES !!
+                    h6(textOutput("save.results_5")),
+                    h5("Créé par:"),
+                    h6("Le comité étudiant de Québec Océan"),
+                    h5("Dépôt Github :"),
+                    tags$a("Code source quiz", href=paste0("https://github.com/david-beauchesne/SurveyTool")),
+                    h5("Lien vers l'outil de réseautage :"),
+                    tags$a("AGA 2016 Réseautage", href=paste0("https://bit.ly/QO2015")),
+                    # Display the page counter text.
+            # !! CHANGE PARAMETER NAME FOR MULTIPLE INSTANCES !!
+                    h5(textOutput("counter_5"))
+                ), #sidebarPanel
+
+                # Show a table summarizing the values entered
+                mainPanel(width = 7,
+                    # Main Action is where most everything is happenning in the
+                    # object (where the welcome message, survey, and results appear)
+            # !! CHANGE PARAMETER NAME FOR MULTIPLE INSTANCES !!
+                    uiOutput("MainAction_5"),
+                    # This displays the action button Next.
+            # !! CHANGE PARAMETER NAME FOR MULTIPLE INSTANCES !!
+                    actionButton("Click.Counter_5", "Suivant")
+                ) #mainPanel
+            ), #tabPanel
+
+            tabPanel('Saint-Laurent - Arctique 2',
+                sidebarPanel(width = 3,
+                    # This is intentionally an empty object.
+            # !! CHANGE PARAMETER NAME FOR MULTIPLE INSTANCES !!
+                    h6(textOutput("save.results_6")),
+                    h5("Créé par:"),
+                    h6("Le comité étudiant de Québec Océan"),
+                    h5("Dépôt Github :"),
+                    tags$a("Code source quiz", href=paste0("https://github.com/david-beauchesne/SurveyTool")),
+                    h5("Lien vers l'outil de réseautage :"),
+                    tags$a("AGA 2016 Réseautage", href=paste0("https://bit.ly/QO2015")),
+                    # Display the page counter text.
+            # !! CHANGE PARAMETER NAME FOR MULTIPLE INSTANCES !!
+                    h5(textOutput("counter_6"))
+                ), #sidebarPanel
+
+                # Show a table summarizing the values entered
+                mainPanel(width = 7,
+                    # Main Action is where most everything is happenning in the
+                    # object (where the welcome message, survey, and results appear)
+            # !! CHANGE PARAMETER NAME FOR MULTIPLE INSTANCES !!
+                    uiOutput("MainAction_6"),
+                    # This displays the action button Next.
+            # !! CHANGE PARAMETER NAME FOR MULTIPLE INSTANCES !!
+                    actionButton("Click.Counter_6", "Suivant")
                 ) #mainPanel
             ), #tabPanel
 
